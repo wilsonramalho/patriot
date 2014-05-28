56cf9d6646e9ac11ed7f5744f9d4acd78f5c2cab
Amora <amora@casulloweb.com.br>
2014-05-28 13:30:43 -0300

    adicao de novos plugins e troca do gulp cache pelo gulp newer

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp          = require('gulp'),
     autoprefixer  = require('gulp-autoprefixer'),
-    cache         = require('gulp-cache'),
+    //cache         = require('gulp-cache'),
+    newer         = require('gulp-newer'),
     clean         = require('gulp-clean'),
     concat        = require('gulp-concat'),
     imagemin      = require('gulp-imagemin'),
@@ -30,7 +31,8 @@ gulp.task('images', function() {
     'source/images/**/*.jpg',
     'source/images/**/*.png'
   ])
-  .pipe(cache(imagemin({interlaced: true, optimizationLevel: 5, progressive: true})))
+  .pipe(newer('build/images/'))
+  .pipe(imagemin({interlaced: true, optimizationLevel: 5, progressive: true}))
   .pipe(gulp.dest('build/images/'))
   .pipe(livereload(server))
   //.pipe(notify({message: 'Images task complete.'}));
@@ -38,25 +40,27 @@ gulp.task('images', function() {
 
 gulp.task('libraries', function() {
   return gulp.src([
-    'bower_components/jquery/dist/jquery.min.js',
+    'bower_components/jquery/jquery.min.js',
     'bower_components/bootstrap/js/transition.js',
-    'bower_components/bootstrap/js/alert.js',
+    //'bower_components/bootstrap/js/alert.js',
     'bower_components/bootstrap/js/button.js',
     'bower_components/bootstrap/js/carousel.js',
-    'bower_components/bootstrap/js/collapse.js',
-    'bower_components/bootstrap/js/dropdown.js',
-    'bower_components/bootstrap/js/modal.js',
-    'bower_components/bootstrap/js/tooltip.js',
-    'bower_components/bootstrap/js/popover.js',
-    'bower_components/bootstrap/js/scrollspy.js',
-    'bower_components/bootstrap/js/tab.js',
-    'bower_components/bootstrap/js/affix.js',
-    'bower_components/jasny-bootstrap/js/fileinput.js',
-    'bower_components/jasny-bootstrap/js/inputmask.js',
+    //'bower_components/bootstrap/js/collapse.js',
+    //'bower_components/bootstrap/js/dropdown.js',
+    //'bower_components/bootstrap/js/modal.js',
+    //'bower_components/bootstrap/js/tooltip.js',
+    //'bower_components/bootstrap/js/popover.js',
+    //'bower_components/bootstrap/js/scrollspy.js',
+    //'bower_components/bootstrap/js/tab.js',
+    //'bower_components/bootstrap/js/affix.js',
+    //'bower_components/jasny-bootstrap/js/fileinput.js',
+    //'bower_components/jasny-bootstrap/js/inputmask.js',
     'bower_components/jasny-bootstrap/js/offcanvas.js',
-    'bower_components/jasny-bootstrap/js/rowlink.js',
-    'bower_components/nprogress/nprogress.js',
-    'source/scripts/**/*.js'
+    //'bower_components/jasny-bootstrap/js/rowlink.js',
+    'bower_components/bootstrap-select/bootstrap-select.js',
+    'bower_components/jquery.validation/dist/jquery.validate.js',
+    'source/scripts/jquery.scripts.js',
+    'source/scripts/jquery.calls.js'
   ])
   .pipe(concat('libraries.js'))
   .pipe(uglify())
@@ -89,7 +93,7 @@ gulp.task('clean', function() {
 });
 
 gulp.task('default', function() {
-  gulp.start('fonts', 'images', 'stylesheets', 'libraries', 'templates');
+  gulp.start('fonts', 'images', 'stylesheets', 'libraries', 'templates', 'custom-js');
 });
 
 gulp.task('watch', ['default'], function() {
