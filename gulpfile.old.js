38c02440f19cba764183140ff733be4a1e3a5efa
Wilson Cordeiro <wilsonramalho@gmail.com>
2016-02-09 12:56:21 -0300

    fix the partials watch problem, replace the minifycss for the cssnano

--- gulpfile.old.js
+++ gulpfile.old.js
@@ -1,52 +1,59 @@
 var
+  $           = require('gulp-load-plugins')(),
   gutil       = require('gulp-util'),
   browserSync = require('browser-sync'),
   del         = require('del'),
-  gulp        = require('gulp'),
-  $           = require("gulp-load-plugins")({
-                  pattern: ['gulp-*', 'gulp.*'],
-                  replaceString: /\bgulp[\-.]/,
-                  lazy: false,
-                  rename: {
-                    'gulp-jade-inheritance' : 'jadeInheritance'
-                  }
-                }),
-  onError = function (err) {  
-    gutil.beep();
-    console.log(err);
-  };
+  gulp        = require('gulp');
+
+// var del = require('del');
+// var browserSync = require('browser-sync');
+
+// var gutil = require('gulp-util');
+
+// var plugins = require("gulp-load-plugins")({
+//     pattern: ['gulp-*', 'gulp.*'],
+//     replaceString: /\bgulp[\-.]/,
+//     lazy: false,
+//     rename: {
+//       'gulp-minify-css' : 'minifycss',
+//       'gulp-if' : 'gulpIf',
+//       'gulp-jade-inheritance' : 'jadeInheritance'
+//     }
+// });
+
+var onError = function (err) {  
+  gutil.beep();
+  console.log(err);
+};
 
 gulp.task('fonts', function() {
-  return gulp.src([
-    'bower_components/fontawesome/fonts/**/*',
-    'source/fonts/**/*'
-  ])
+  gulp.src('source/fonts/**/*')
   .pipe($.plumber({
     errorHandler: onError
   }))
-  .pipe(gulp.dest('build/fonts/'))
+  .pipe(gulp.dest('build/assets/fonts'));
 });
 
 gulp.task('images', function() {
-  return gulp.src([
+  gulp.src([
     'source/images/**/*.gif',
     'source/images/**/*.ico',
-    'source/images/**/*.jpg',
     'source/images/**/*.jpeg',
-    'source/images/**/*.svg',
-    'source/images/**/*.png'
+    'source/images/**/*.jpg',
+    'source/images/**/*.png',
+    'source/images/**/*.svg'
   ])
   .pipe($.imagemin({
     interlaced: true,
     optimizationLevel: 5,
     progressive: true
   }))
-  .pipe(gulp.dest('build/images'));
+  .pipe(gulp.dest('build/assets/images'));
 });
 
 gulp.task('libraries', function() {
   return gulp.src([
-    'bower_components/jquery/jquery.min.js',
+    'bower_components/jquery/dist/jquery.min.js',
     //'bower_components/bootstrap/js/transition.js',
     //'bower_components/bootstrap/js/alert.js',
     //'bower_components/bootstrap/js/button.js',
@@ -65,12 +72,12 @@ gulp.task('libraries', function() {
     //'bower_components/jasny-bootstrap/js/rowlink.js',
     'source/scripts/jquery.scripts.js'
   ])
-  .pipe($.plumber({
+  .pipe(plugins.plumber({
     errorHandler: onError
   }))
   .pipe($.concat('libraries.js'))
   .pipe($.uglify())
-  .pipe(gulp.dest('build/scripts/'))
+  .pipe(gulp.dest('build/assets/scripts'));
 });
 
 gulp.task('stylesheets', function() {
@@ -80,44 +87,42 @@ gulp.task('stylesheets', function() {
   }))
   .pipe($.less())
   .pipe($.autoprefixer('last 2 versions', 'ie 8', 'ie 9'))
-  .pipe($.cssnano())
-  .pipe(gulp.dest('build/stylesheets/'))
+  .pipe($.minifyCss({keepSpecialComments: 0, removeEmpty: true}))
+  .pipe(gulp.dest('build/assets/stylesheets'));
 });
 
 gulp.task('templates', function() {
-  return gulp.src(['source/**/!(_)*.jade'])
-  .pipe($.changed('build', { extension: '.html'}))
-  .pipe($.jadeInheritance({ basedir: 'source'}))
+  gulp.src(['!source/partials/**/!(_)*.jade', 'source/**/!(_)*.jade'])
+  .pipe($.changed('build', { extension: '.html' }))
   .pipe($.plumber())
-  .pipe($.jade({
-    pretty: true
-  }))
+  .pipe($.jade({ pretty: true }))
   .pipe(gulp.dest('build'));
 });
 
-gulp.task('clear-partials', ['templates'], function() {
-  del('build/partials/')
+gulp.task('build', function() {
+  gulp.start(
+    'fonts',
+    'images',
+    'libraries',
+    'stylesheets',
+    'templates'
+  );
 });
 
 gulp.task('clear', function() {
   del('build/*')
 });
 
-gulp.task('setWatch', function() {
-  global.isWatching = true;
-});
+gulp.task('watch', function() {
 
-// Watch
-
-gulp.task('watch', ['setWatch'],  function() {
   gulp.watch('source/fonts/**/*', ['fonts']);
   gulp.watch('source/images/**/*', ['images']);
   gulp.watch('source/scripts/**/*.js', ['libraries']);
   gulp.watch('source/stylesheets/**/*.less', ['stylesheets']);
-  gulp.watch('source/**/*.jade', ['clear-partials']);
+  gulp.watch('source/**/*.jade', ['templates']);
+
   browserSync.init('build/**/*', {
-    server: {
-      baseDir: 'build'
-    }
+    server: { baseDir: 'build' }
   });
+
 });
